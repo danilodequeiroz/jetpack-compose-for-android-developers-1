f76e78ff0c7983af27a71479d4378c966bb06551
danilodequeiroz <danilo.aq@gmail.com>
2025-11-03 16:25:06 -0300

    spring animation on show more list button.

--- composeApp/src/androidMain/kotlin/com/github/danilodequeiroz/jetpack_compose_for_android_developers/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/github/danilodequeiroz/jetpack_compose_for_android_developers/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -73,15 +74,24 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = rememberSaveable { mutableStateOf(false) }
-//    val extraPadding = if(expanded.value) 48.dp else 0.dp
-    val dp = if (expanded.value) 48.dp else 0.dp
-    val extraPadding by animateDpAsState(
-        targetValue = dp,
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val valueInDP = if (expanded) 48.dp else 0.dp
+
+    val tweenExtraPadding by animateDpAsState(
+        targetValue = valueInDP,
         animationSpec = tween(
             durationMillis = 2000
         )
     )
+
+    val springExtraPadding by animateDpAsState(
+        targetValue = valueInDP,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(
@@ -96,18 +106,17 @@ fun Greeting(name: String) {
             Column(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 modifier = Modifier.weight(weight = 1f)
-                    .padding(
-                        bottom = extraPadding)
+                        .padding(bottom = springExtraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             ElevatedButton(
                 onClick = {
-                    expanded.value = expanded.value.not()
+                    expanded = expanded.not()
                 }) {
                 Text(
-                    text = if (expanded.value) "Show less" else "Show more"
+                    text = if (expanded) "Show less" else "Show more"
                 )
             }
         }
